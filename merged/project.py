f04357036b3b15aee5e05dbbad2abc48b4ae64a6
Adam <adamgiebl@gmail.com>
2019-12-08 02:06:54 +0100

    RESTRUCTURING

--- merged/project.py
+++ merged/project.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
 from termcolor import colored
 import datetime as dt
+from console_progressbar import ProgressBar
+from timeloop import Timeloop
+import time
 
 # -----CLASS FOR PARSING-----
 
+tl = Timeloop()
+progress = 0    
+
 
 class Parser:
-    """The class's docstring"""
 
-    def __init__(self, path, encoding):
+    def __init__(self, path, encoding='utf8'):
         self.path = path
         self.encoding = encoding
         self.headers = []
@@ -16,25 +21,47 @@ class Parser:
 
     def getData(self, separator, lineSeparator):
         """The method's docstring"""
-        file = open(self.path, encoding=self.encoding)
-        txt = file.read()
+        finished = False
+        t = time.process_time()
+        """
+        @tl.job(interval=dt.timedelta(seconds=0.01))
+        def sample_job_every_2s():
+            if not finished:
+                global progress
+                progress += 1
+                pb.print_progress_bar(progress)
+        
+        tl.start()
+        sample_job_every_2s()
+        time.sleep(0.1)"""
+
+        txt = ""
+        with open(self.path, encoding=self.encoding) as file:
+            txt = file.read()
+
+        time_read = time.process_time() - t
 
         # split data into lines
         arr = txt.split(lineSeparator)
 
         # get headers from the first line
         self.headers = arr[0].split(separator)
-
+        
         # loop over remaining lines and put them into list of dictionaries
         for line in arr[1:]:
             split = line.split(separator)
 
-            # continue parsing only if the data is correct (same number of items as headers)
+            # continue parsing only if the data is correct
+            # (same number of items as headers)
             if len(split) == len(self.headers):
                 temp = []
                 for i in range(len(self.headers)):
                     temp.append(split[i])
                 self.data.append(dict(zip(self.headers, temp)))
+        time_parse = time.process_time() - t
+        print(colored('CSV successfully parsed', 'green'))
+        print(colored(f'Time to read: {time_read}s', 'blue'))
+        print(colored(f'Time to parse: {time_parse}s', 'blue'))
         return self.data
 
 
@@ -49,16 +76,14 @@ def getDateObject(stringDate):
 
 
 def getAverageFromList(list):
-    # calculate the average by dividing the sum of numbers by quantity of numbers
+    # calculate the average by dividing the sum of numbers by quantity
     return sum(list) / len(list)
 
 
-def getSpeedPerDates(city, data):
-    city = filterByCity(city, data)
+def getSpeedsPerDate(data):
     dates = []
     speeds = []
-    for dict in city:
-        print(dict['download'])
+    for dict in data:
         dates.append(dict['date'])
         speeds.append(float(dict['download']))
 
@@ -76,7 +101,7 @@ def askUser(path):
         return res.lower()
 
 
-def getAverageSpeedPerMonth(city, data):
+def getAverageSpeedPerMonths(city, data):
     downloadSpeeds = []
     dates = []
     months = []
@@ -106,7 +131,7 @@ def getAverageSpeedPerMonth(city, data):
     return {'x': x, 'y': y}
 
 
-def getAvgSpeedInMonth(selectedMonth, data):
+def getAvgSpeedPerMonth(selectedMonth, data):
     somethings = {}
 
     for dict in data:
@@ -122,41 +147,81 @@ def getAvgSpeedInMonth(selectedMonth, data):
     return avgSpeed
 
 
+def getPlotConfig(label_x="x", label_y="y", ticks=10):
+    if darkMode:
+        plt.style.use('dark_background')
+    else:
+        plt.style.use('ggplot')
+    plt.tight_layout()
+    plt.xlabel(label_x)
+    plt.ylabel(label_y)
+    plt.xticks(ticks)
+    plt.legend(loc='best')
+    plt.show()
+
+
 # -----EXECUTION-----
+
+pb = ProgressBar(total=10, prefix='Here', suffix='Now', decimals=1, length=10, fill='X', zfill='-')
 
 parser = Parser('data.csv', 'utf8')
 
-data = parser.getData(separator=',', lineSeparator='\n')
+data = []
 
 choice = askUser('menu.txt')
 
+darkMode = False
+
+
 while choice != 'q':
     if choice == 1:
-        print(colored('CSV successfully parsed', 'green'))
-        input()
+        data = parser.getData(separator=',', lineSeparator='\n')
+        input("Click ENTER to continue...")
     elif choice == 2:
         cityData = filterByCity('Fanø', data)
-        augustAvg = getAvgSpeedInMonth('August', cityData)
-        septemberAvg = getAvgSpeedInMonth('September', cityData)
-        print('Average download speed for the months of August and September in Fanø is:')
-        print(colored((augustAvg + septemberAvg) / 2, 'green'))
-        input()
+        augustAvg = getAvgSpeedPerMonth('August', cityData)
+        septemberAvg = getAvgSpeedPerMonth('September', cityData)
+        print('Average download speed for August and September in Fanø is:')
+        print(colored(getAverageFromList([augustAvg, septemberAvg]), 'green'))
+        input("Click ENTER to continue...")
     elif choice == 3:
-        dick = getAverageSpeedPerMonth('Ballerup', data)
-        dick2 = getAverageSpeedPerMonth('Copenhagen', data)
-        plt.plot(dick['x'], dick['y'], 'r', label="Bellerup")
-        plt.plot(dick2['x'], dick2['y'], 'b', label="Copenhagen")
-        plt.xticks(range(len(list(dick['x']))+1))
-        plt.xlabel("Months")
-        plt.ylabel("Avg. Download speed")
-        plt.legend(loc='best')
-        plt.show()
+        average_speed_ballerup = getAverageSpeedPerMonths('Ballerup', data)
+        average_speed_copenhagen = getAverageSpeedPerMonths('Copenhagen', data)
+        plt.plot(
+            average_speed_ballerup['x'],
+            average_speed_ballerup['y'],
+            'red',
+            label="Bellerup"
+        )
+        plt.plot(
+            average_speed_copenhagen['x'],
+            average_speed_copenhagen['y'],
+            'blue',
+            label="Copenhagen"
+        )
+        getPlotConfig(
+            label_x="Month",
+            label_y="Avg. Download speed",
+            ticks=range(len(list(average_speed_ballerup['x']))+1)
+        )
     elif choice == 4:
-        dick = getAverageSpeedPerMonth('Lolland', data)
-        plt.bar(dick['x'], dick['y'], label="Bellerup")
-        plt.xticks(range(len(list(dick['x']))+1))
-        plt.xlabel("Months")
-        plt.ylabel("Avg. Download speed")
-        plt.legend(loc='best')
-        plt.show()
+        average_speed_lolland = getAverageSpeedPerMonths('Lolland', data)
+        plt.bar(
+            average_speed_lolland['x'],
+            average_speed_lolland['y'],
+            label="Lolland"
+        )
+        getPlotConfig(
+            label_x="Month",
+            label_y="Avg. Download speed",
+            ticks=range(len(list(average_speed_lolland['x']))+1)
+        )
+    elif choice == 5:
+        darkMode ^= True
+        print("Dark mode is: ", end=" ")
+        if darkMode:
+            print(colored("ON", "green"))
+        else:
+            print(colored("OFF", "red"))
+        input("Click ENTER to continue...")
     choice = askUser('menu.txt')
--- src/menu.py
+++
@@ -1,8 +0,0 @@
-def askUser(path):
-    file = open(path, encoding='utf8')
-    txt = file.read()
-    print(txt)
-    res = input()
-    if res.isdigit():
-        return int(res)
-    else: return res.lower()
